14ff5845a0b1a30905186c476f5b34267ba626e4
DefectingCat <i@defect.ink>
2022-12-16 16:06:10 +0800

    Format with prettier
    add prettier config

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,28 +3,28 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { Feather, FontAwesome } from "@expo/vector-icons";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather, FontAwesome } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
   DarkTheme,
   DefaultTheme,
   NavigationContainer,
-} from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as React from "react";
-import { ColorSchemeName } from "react-native";
-import useColorScheme from "../hooks/useColorScheme";
-import ModalScreen from "../screens/ModalScreen";
-import NotFoundScreen from "../screens/NotFoundScreen";
-import TabOneScreen from "../screens/HomeScreen";
-import TabTwoScreen from "../screens/TabTwoScreen";
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react';
+import { ColorSchemeName } from 'react-native';
+import useColorScheme from '../hooks/useColorScheme';
+import ModalScreen from '../screens/ModalScreen';
+import NotFoundScreen from '../screens/NotFoundScreen';
+import TabOneScreen from '../screens/HomeScreen';
+import TabTwoScreen from '../screens/TabTwoScreen';
 import {
   RootStackParamList,
   RootTabParamList,
   RootTabScreenProps,
-} from "../types";
-import LinkingConfiguration from "./LinkingConfiguration";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+} from '../types';
+import LinkingConfiguration from './LinkingConfiguration';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 export default function Navigation({
   colorScheme,
@@ -34,7 +34,7 @@ export default function Navigation({
   return (
     <NavigationContainer
       linking={LinkingConfiguration}
-      theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
     >
       <RootNavigator />
     </NavigationContainer>
@@ -58,9 +58,9 @@ function RootNavigator() {
       <Stack.Screen
         name="NotFound"
         component={NotFoundScreen}
-        options={{ title: "Oops!" }}
+        options={{ title: 'Oops!' }}
       />
-      <Stack.Group screenOptions={{ presentation: "modal" }}>
+      <Stack.Group screenOptions={{ presentation: 'modal' }}>
         <Stack.Screen name="Modal" component={ModalScreen} />
       </Stack.Group>
     </Stack.Navigator>
@@ -82,8 +82,8 @@ function BottomTabNavigator() {
       <MBottomTab.Screen
         name="Home"
         component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<"Home">) => ({
-          title: "Home",
+        options={({ navigation }: RootTabScreenProps<'Home'>) => ({
+          title: 'Home',
           tabBarIcon: ({ color }) => <FeatherIcon name="home" color={color} />,
         })}
       />
@@ -100,7 +100,7 @@ function BottomTabNavigator() {
 }
 
 function FeatherIcon(props: {
-  name: React.ComponentProps<typeof Feather>["name"];
+  name: React.ComponentProps<typeof Feather>['name'];
   color?: string;
 }) {
   return (
@@ -120,7 +120,7 @@ function FeatherIcon(props: {
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
+  name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
   return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
