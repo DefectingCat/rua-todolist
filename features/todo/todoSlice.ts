a3ebe947e25158b8717c65a5f4d04b3ac31b3807
DefectingCat <i@defect.ink>
2022-12-16 16:24:46 +0800

    Add todo slice

---
+++ features/todo/todoSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from 'app/store';
+
+export interface SingleTodo {
+  id: string;
+  content: string;
+  isDone: boolean;
+}
+// Define a type for the slice state
+export interface TodoState {
+  todos: SingleTodo[];
+}
+
+// Define the initial state using that type
+const initialState: TodoState = {
+  todos: [],
+};
+
+export const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  // Dispatch "event" not "setter"
+  reducers: {},
+});
